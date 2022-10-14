40a706bbee6cefb3edf5a8ae3650dc4a2eb68a59
tobioketch <96707752+tobioketch@users.noreply.github.com>
2022-10-13 22:58:48 -0500

    changed initial numeric summaries to five number summary
    changed colors of the plots to skyblue
    
    removed auto generated title for the histogram to none
    
    changed function name from ds to desc

--- R/ds.R
+++ R/ds.R
@@ -1,21 +1,22 @@
-#' Creates histogram, box plot and numeric summary statistics
+#' Creates histogram, box plot and computes five number summary statistics
 #' @export
 #' @param x numeric variable
 #' @importFrom graphics hist boxplot par
 #' @importFrom grDevices rainbow
+#' @importFrom  stats setNames fivenum
 
-ds <- function(x) {
+desc <- function(x) {
     # 1 row and 2 columns
     par(mfrow = c(1, 2))
     # Histogram
-    hist(x, col = rainbow(30))
+    hist(x, col = "skyblue", main = "" )
     # box plot
-    boxplot(x, col = 'green')
+    boxplot(x, col = 'skyblue')
     par(mfrow = c(1, 1))
 
-    # summary statistics
-    data.frame(min = min(x),
-               mean = mean(x),
-               max = max(x))
-
+    # five number summary
+    stats::setNames(
+        stats::fivenum(x),
+        nm =c("min", "Q1", "Q2" , "Q3", "max")
+    )
 }
